a5abd65e9382b684e510db12eac1d537aa64e65b
Rebecca Kukshtel <rkukshtel@protonmail.com>
2021-09-22 15:25:35 -0400

    kotlin runs 👍

--- klox/build.gradle.kts
+++ klox/build.gradle.kts
@@ -13,6 +13,7 @@ repositories {
 
 dependencies {
     testImplementation(kotlin("test"))
+    implementation(kotlin("script-runtime"))
 }
 
 tasks.test {
---
+++ klox/src/main/kotlin/main.kt
@@ -0,0 +1,5 @@
+fun main() {
+    println("Oh, didn't see you there! What's your name?")
+    val name = readLine()
+    println("Hello, $name!!")
+}
