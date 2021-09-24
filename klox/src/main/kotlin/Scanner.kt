4c4f35926381fef5347e34e5aa9c80ccb9a25d22
Rebecca Kukshtel <rkukshtel@protonmail.com>
2021-09-24 13:07:10 -0400

    implement mvp scanTokens

--- klox/src/main/kotlin/Scanner.kt
+++ klox/src/main/kotlin/Scanner.kt
@@ -3,6 +3,7 @@ package me.bink.klox
 class Scanner constructor(private val source: String) {
 
     private val tokens = ArrayList<Token>()
+    private val sourceChars = source.toCharArray()
 
     // offset into the string pointing to the first character of the lexeme
     private var start = 0
@@ -25,11 +26,35 @@ class Scanner constructor(private val source: String) {
         return tokens
     }
 
-    private fun scanToken() {
-        TODO("Not yet implemented")
+    private fun isAtEnd(): Boolean {
+        return current >= source.length
     }
 
-    private fun isAtEnd(): Boolean {
-        return false
+    private fun scanToken() {
+        when (advance()) {
+            '(' -> addToken(TokenType.LEFT_PAREN)
+            ')' -> addToken(TokenType.RIGHT_PAREN)
+            '{' -> addToken(TokenType.LEFT_BRACE)
+            '}' -> addToken(TokenType.RIGHT_BRACE)
+            ',' -> addToken(TokenType.COMMA)
+            '.' -> addToken(TokenType.DOT)
+            '-' -> addToken(TokenType.MINUS)
+            '+' -> addToken(TokenType.PLUS)
+            ';' -> addToken(TokenType.SEMICOLON)
+            '*' -> addToken(TokenType.STAR)
+        }
+    }
+
+    private fun advance(): Char {
+        current++
+        // TODO: check bounds?
+        return sourceChars[current - 1]
+    }
+
+    private fun addToken(tokenType: TokenType, literal: Any? = null) {
+        // start inclusive, end exclusive
+        val text = source.substring(start, current)
+
+        tokens.add(Token(tokenType, text, literal, line))
     }
 }
